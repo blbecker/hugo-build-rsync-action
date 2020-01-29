751de2a7716d6da2db3061c86eadea50c62fd493
Benjamin Becker <benjamin.becker@athenitas.com>
2020-01-28 20:01:43 -0500

    Make git branch and sha available to builds.

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,9 @@ echo "${INPUT_DEPLOYMENT_KEY}" | base64 -d >> "/root/.ssh/id_rsa" && \
 
 cd "${PROJECT_DIR}" && rm -rf public/
 
+export BRANCH=$(git rev-parse --abbrev-ref HEAD)
+export GIT_SHA=$(git rev-parse --verify HEAD --short)
+
 git submodule init && git submodule update --recursive
 
 hugo
